5e7936f348980389c110a821de0a9448f9698658
Prasad Deshpande <prasad.deshpande65@yahoo.com>
2017-08-13 16:29:11 -0700

    modifications to test model

--- app/tests/userridepref.server.model.test.js
+++ app/tests/userridepref.server.model.test.js
@@ -17,28 +17,17 @@ var user, userridepref;
  * Unit tests
  */
 describe('Userridepref Model Unit Tests:', function() {
-	beforeEach(function(done) {
-		user = new User({
-			firstName: 'Full',
-			lastName: 'Name',
-			displayName: 'Full Name',
-			email: 'test@test.com',
-			username: 'username',
-			password: 'password'
-		});
-
-		user.save(function() { 
-			userridepref = new Userridepref({
-				// Add model fields
-				// ...
-			});
-
-			done();
-		});
-	});
+	
 
 	describe('Method Save', function() {
 		it('should be able to save without problems', function(done) {
+			
+			userridepref = new Userridepref({
+				// Add model fields
+				// ...
+				'userid': '598e913d50fbe87819f14beb',
+				'ridepref': 'ride'
+			});
 			return userridepref.save(function(err) {
 				should.not.exist(err);
 				done();
