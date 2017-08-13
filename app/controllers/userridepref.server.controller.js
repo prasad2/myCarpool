969fe2fe105d2a7c6c82fcf0bef41d67ab983555
Prasad Deshpande <prasad.deshpande65@yahoo.com>
2017-08-13 16:27:55 -0700

    Added new controller for user ride pref

---
+++ app/controllers/userridepref.server.controller.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    _ = require('lodash');
+
+/**
+ * Create a Userridepref
+ */
+exports.create = function(req, res) {
+
+};
+
+/**
+ * Show the current Userridepref
+ */
+exports.read = function(req, res) {
+
+};
+
+/**
+ * Update a Userridepref
+ */
+exports.update = function(req, res) {
+
+};
+
+/**
+ * Delete an Userridepref
+ */
+exports.delete = function(req, res) {
+
+};
+
+/**
+ * List of Userrideprefs
+ */
+exports.list = function(req, res) {
+
+};
